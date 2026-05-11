3ce6ae8aada7cbb5433194d6c5c67b7207cbe0eb
smartbox2021 <info@smartboxselfstorage.uk>
2026-05-11 10:15:47 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['cheerio', 'nodemailer'],
-  },
+  serverExternalPackages: ['cheerio', 'nodemailer'],
 }
 
 module.exports = nextConfig
